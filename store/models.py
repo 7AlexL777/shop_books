646efda2d032596ebd25e92cc90addaa1365cd45
alexL <litvinaualex@gmail.com>
2022-05-14 10:35:11 +0300

    add filters, search, ordering

--- store/models.py
+++ store/models.py
@@ -5,3 +5,8 @@ class Book(models.Model):
     objects = None
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
+    author_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'ID{self.id}: {self.name}'
+
